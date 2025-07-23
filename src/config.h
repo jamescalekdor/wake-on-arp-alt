175a4335e0175a0f9e8658ab9df8aa7a66e27582
jamescalekdor <136054848+jamescalekdor@users.noreply.github.com>
2025-07-23 18:24:00 -0400

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,26 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <netinet/in.h>
+#include <net/ethernet.h>
+#include <net/if.h>
+
+#define MAX_TARGETS 10
+#ifndef CONFIG_FILE
+#define CONFIG_FILE "/etc/wake-on-arp-alt.conf"
+#endif
+
+struct config {
+    struct in_addr broadcast_ip;
+    char net_device[IF_NAMESIZE];
+    char lan_device[IF_NAMESIZE];
+    int subnet;
+    bool allow_gateway;
+    int num_targets;
+    struct in_addr target_ip[MAX_TARGETS];
+    struct ether_addr target_mac[MAX_TARGETS];
+};
+
+int load_config(struct config *conf);
+
+#endif
